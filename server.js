f33d4f21ab596455ada6b8a4a4973f5c248a6193
ojasgupta-25 <ojasgupta25@gmail.com>
2023-03-08 17:05:12 -0600

    made server/fake db
    made server and temp variables to act like the database

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+// run server typing "node server.js" in your terminal when in this directory 
+// open the website by typing localhost:3000 on browser
+// every time you change code you need to relaunch the server 
+// to get around it you can type in "sudo npm install -g nodemon"
+// than to run the server you can say nodemon server.js instead of node server.js
+const express = require('express')
+const app = express()
+// so that the css file is sent to the front end aswell
+app.use(express.static(__dirname + '/public/'));
+// port we are using 
+const port = 3000
 
+// dummy variables (fake data base)
+var db_username = "john.doe";
+var db_add1 = "3100 university dr";
+var db_add2 = "apt 1234";
+var db_city= "houston";
+var db_state = "Texas";
+var db_zip = 75078;
+var db_quote_arr= [512,432,234,654,677];
+// end of fake database 
+
+// '/' is the default route when someone opens the website on local port 3000
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+ "/index.html")
+})
+
+
+// REMOVE THIS WHEN IMPLEMENTING SECOND PAGE| THIS IS ONLY FOR TESTING
+// USERS SHOULD NOT HAVE DIRECT ACCESS TO SECOND PAGE
+app.get('/user', (req, res) => {
+    res.sendFile(__dirname+ "/index2.html")
+  })
+// END OF REMOVE 
+
+
+// console log that you started the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
